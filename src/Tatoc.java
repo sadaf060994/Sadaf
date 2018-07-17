2f2b61333b935194db0300433b22a9308185343f
sadaf060994 <sadaf060994@gmail.com>
2018-07-17 10:07:42 +0530

    Done

--- src/Tatoc.java
+++ src/Tatoc.java
@@ -1,13 +1,10 @@
 import java.util.concurrent.TimeUnit;
-
-import org.apache.http.cookie.ClientCookie;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
-import com.gargoylesoftware.htmlunit.util.Cookie;
+import org.openqa.selenium.Cookie;
 
 
 public class Tatoc {
@@ -17,32 +14,32 @@ public class Tatoc {
 		String color1, color2, token;
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sadafansari\\workspace\\Demo1\\lib\\chromedriver.exe");
 	    WebDriver driver = new ChromeDriver();
-	    driver.get("http://10.0.1.86/tatoc/basic/drag");   
+	    driver.get("http://10.0.1.86/tatoc");   
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    driver.findElement(By.xpath("/html/body/div/div[2]/a[1]")).click();
 //	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//	    driver.findElement(By.xpath("/html/body/div/div[2]/a[1]")).click();
+	    driver.findElement(By.className("greenbox")).click();
 //	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//	    driver.findElement(By.className("greenbox")).click();
-//	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//	    driver.switchTo().frame("main");
-//	    color1 = driver.findElement(By.id("answer")).getAttribute("class");
-//	    System.out.println("Color of Box 1 = " + color1);
-//	    driver.switchTo().frame("child");
-//	    color2 = driver.findElement(By.id("answer")).getAttribute("class");
-//	    System.out.println("Color of Box 2 = " + color2);
-//	    driver.switchTo().defaultContent();
-//	    
-//	    while(!color1.equals(color2) ) {
-//	    	driver.switchTo().frame("main");
-//	    	driver.findElement(By.xpath("/html/body/center/a[1]")).click();
-//	    	driver.switchTo().frame("child");
-//	    	color2 = driver.findElement(By.id("answer")).getAttribute("class");	
-//	    	System.out.println("Color of Box 2 = "+ color2);
-//	    	driver.switchTo().defaultContent();
-//		}
-//	        driver.switchTo().defaultContent();
-//	    	driver.switchTo().frame("main");
-//	    	driver.findElement(By.xpath("/html/body/center/a[2]")).click();	
-//	    	System.out.println("Color of Box 1 = " + color1 + " is equal to Color of Box 2 = " + color2);
+	    driver.switchTo().frame("main");
+	    color1 = driver.findElement(By.id("answer")).getAttribute("class");
+	  //  System.out.println("Color of Box 1 = " + color1);
+	    driver.switchTo().frame("child");
+	    color2 = driver.findElement(By.id("answer")).getAttribute("class");
+	  //  System.out.println("Color of Box 2 = " + color2);
+	    driver.switchTo().defaultContent();
+	    
+	    while(!color1.equals(color2) ) {
+	    	driver.switchTo().frame("main");
+	    	driver.findElement(By.xpath("/html/body/center/a[1]")).click();
+	    	driver.switchTo().frame("child");
+	    	color2 = driver.findElement(By.id("answer")).getAttribute("class");	
+	    //	System.out.println("Color of Box 2 = "+ color2);
+	    	driver.switchTo().defaultContent();
+		}
+	        driver.switchTo().defaultContent();
+	    	driver.switchTo().frame("main");
+	    	driver.findElement(By.xpath("/html/body/center/a[2]")).click();	
+	   // 	System.out.println("Color of Box 1 = " + color1 + " is equal to Color of Box 2 = " + color2);
 	    	WebElement From=driver.findElement(By.id("dragbox"));	
 	    	WebElement To=driver.findElement(By.id("dropbox"));
 	    	Actions act = new Actions(driver);
@@ -57,10 +54,12 @@ public class Tatoc {
 	    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	    	driver.findElement(By.xpath("/html/body/div/div[2]/a[2]")).click();
 	    	driver.findElement(By.xpath("/html/body/div/div[2]/a[1]")).click();
-	    	token = driver.findElement(By.id("token")).getText();
-	    	System.out.println("Token = " + token);
-	    	Cookie ck = new Cookie("Token", token, winHandleBefore);
-	    	driver.findElement(By.xpath("/html/body/div/div[2]/a[2]")).click();
+	        token = driver.findElement(By.id("token")).getText();
+	        String[] splited = token.split("\\s+");
+	        System.out.println("Token = " + splited[1]);
+	        Cookie ck = new Cookie("Token", splited[1]);
+	        driver.manage().addCookie(ck);
+	        driver.findElement(By.xpath("/html/body/div/div[2]/a[2]")).click();
 	    	
 	}
 
